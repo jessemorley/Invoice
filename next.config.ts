67632b4a0eee6b5d527bfbf54379c40caa23af6a
Jesse Morley <jessmorley@gmail.com>
2026-05-03 12:27:56 +1000

    Add 10.0.0.15 to allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['10.0.0.54'],
+  allowedDevOrigins: ['10.0.0.54', '10.0.0.15'],
   cacheComponents: true,
 };
 
