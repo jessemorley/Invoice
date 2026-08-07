937d921b7ed56ff52c7fc8416d11269a249bd715
Jesse Morley <jessmorley@gmail.com>
2026-08-07 18:16:52 +1000

    chore: allow dev origin 10.0.0.16

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['10.0.0.54', '10.0.0.15'],
+  allowedDevOrigins: ['10.0.0.54', '10.0.0.15', '10.0.0.16'],
   cacheComponents: true,
 };
 
